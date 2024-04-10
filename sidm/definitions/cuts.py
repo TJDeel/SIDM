00460d21fd1249a754f4925653a40284003101d4
TJDeel <m261524@usna.edu>
2024-04-10 04:07:31 +0000

    4 Photon Selections Made
    Barrel photons, barrel matches photons, loose barrel photons, and loose barrel matches photons added to selections.yaml. Cuts that were already defined were used ("Keep all evts", "pT > 20GeV", "|eta| < 2.5", and "looseID"). "dR(e, A) < 0.5" copied from electron cuts and put under photon cuts (needed for matched selections).

--- sidm/definitions/cuts.py
+++ sidm/definitions/cuts.py
@@ -53,6 +53,7 @@ obj_cut_defs = {
     "photons":{
         "pT > 20 GeV": lambda objs: objs["photons"].pt > 20,
         "|eta| < 2.5": lambda objs: abs(objs["photons"].scEta) < 2.5, # fixme: do we want eta or scEta (which is only available in v4)?
+        "dR(e, A) < 0.5": lambda objs: dR(objs["photons"], objs["genAs_toE"]) < 0.5,
         #Loose ID = bit 0
         "looseID": lambda objs: check_bit(objs["photons"].idResults,0),
     },
